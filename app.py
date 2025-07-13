be4940bad3ffa653b120c8e1f248c1b2b395dbc3
sayancoding <sayanmaity007@gmail.com>
2025-07-13 14:30:34 +0530

    Problem Solver Agent Added

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from autogen_ext.code_executors.docker import DockerCommandLineCodeExecutor
-from autogen_agentchat.agents import CodeExecutorAgent
+from autogen_agentchat.agents import CodeExecutorAgent,AssistantAgent
 from autogen_agentchat.messages import TextMessage
 from autogen_core import CancellationToken
-import asyncio
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_agentchat.conditions import TextMentionTermination
+from autogen_agentchat.base import TaskResult
 
+import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv('GOOGLE_API_KEY')
+if not GEMINI_API_KEY:
+    raise ValueError("GEMINI API Key is not pres")
+
+llm_client = OpenAIChatCompletionClient(model='gemini-2.5-flash',api_key=GEMINI_API_KEY)
 
 async def main():
 
@@ -17,23 +30,42 @@ async def main():
         code_executor=docker
     )
 
-    task = TextMessage(
-        content='''Here is some code
-```python
-print('Hello world')
-```
-    ''',    
-        source="user",
+    problemSolverAgent = AssistantAgent(
+        name = "ProblemSolverAgent",
+        description="An assistant agent that help to solve DSA problem & write code",
+        model_client= llm_client,
+        system_message="""
+        you are the code solver agent that very expert in data structure and algorithm and solve any that related coding problem.
+        you will be working with code executor agent to execute code.
+        you will given problem or task , you will solve.
+        At first explain your approach and then give the answer with in simple block of code in python language only.
+        then pass block of code to code-executor-agent to execute along with 2 test cases.
+        if the code is executed successfully and test cases are passed, then you have the result.
+
+        in the end if all are satisfied , then you have to say "STOP" to end conversation.
+    """
     )
 
-    await docker.start()
+    termination_call = TextMentionTermination("STOP")
+
+    team = RoundRobinGroupChat(
+        participants= [problemSolverAgent, code_executor_agent],
+        termination_condition= termination_call,
+        max_turns=10
+    )
 
     try:
-        result = await code_executor_agent.on_messages(
-            messages= [task],
-            cancellation_token= CancellationToken()
-        ) 
-        print("Result : ",result.chat_message.content) # type: ignore
+        task = " Write a function for fibonacci series "
+        await docker.start()
+
+        async for message in team.run_stream(task=task):
+            if(isinstance(message,TextMessage)):
+                print("==="*20)
+                print(f" {message.source} :: {message.content}") # type: ignore
+                print("==="*20)
+            elif(isinstance(message, TaskResult)):
+                print(f"Stopping Conversation for : {message.stop_reason}")
+
     except Exception as ex:
         print(f"ERROR :: {ex}")
     finally:
